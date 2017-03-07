c1ef052e6a7461532b631c4d5ddff024676fcd5f
MagicBowen <e.bowen.wang@icloud.com>
2017-03-07 16:31:17 +0800

    fix bug of cpp visitor

--- dsl/cpp-syntax/PromiseSyntaxVisitor.cpp
+++ dsl/cpp-syntax/PromiseSyntaxVisitor.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
+#include <string>
+#include <set>
+#include <exception>
 #include "antlr4-runtime.h"
 #include "PromiseLexer.h"
 #include "PromiseParser.h"
-#include "PromiseVisitor.h"
-#include "PromiseListener.h"
+#include "PromiseBaseVisitor.h"
 
 using namespace antlr4;
 
-struct PromiseSyntaxVisitor : PromiseVisitor
+struct PromiseSyntaxVisitor : PromiseBaseVisitor
 {
+private:
+    antlrcpp::Any visitFactdef(PromiseParser::FactdefContext *context) override
+    {
+        auto id = context->ID()->getText();
+        if(isExisted(id)) throw "Error: duplicate fact id " + id;
+        factIdTable.insert(id);
+        return 1;
+    }
 
+    antlrcpp::Any visitFactId(PromiseParser::FactIdContext *context) override
+    {
+        return checkFactIdDefined(*context);
+    }
+
+    antlrcpp::Any visitClosureFactId(PromiseParser::ClosureFactIdContext *context) override
+    {
+        return checkFactIdDefined(*context);
+    }
+
+private:
+    bool isExisted(const std::string& id) const
+    {
+        return !(factIdTable.find(id) == factIdTable.end());
+    }
+
+    template<typename CONTEXT>
+    antlrcpp::Any checkFactIdDefined(CONTEXT& ctxt)
+    {
+        auto id = ctxt.ID()->getText();
+        if(!isExisted(id)) throw "Error: missing fact " + id;
+        return 0;
+    }
+
+private:
+    std::set<std::string> factIdTable;
 };
 
 int main(int argc, const char* argv[]) 
@@ -21,12 +57,10 @@ int main(int argc, const char* argv[])
     CommonTokenStream tokens(&lexer);
     PromiseParser parser(&tokens);
 
-    // Ref<tree::ParseTree> tree = parser.prog();
-    // Ref<TreeShapeListener> listener(new TreeShapeListener());
-    // tree::ParseTreeWalker::DEFAULT->walk(listener, tree);
-
     tree::ParseTree *tree = parser.prog();
-    std::cout << tree->toStringTree(&parser) << std::endl;
+    PromiseSyntaxVisitor visitor;
+    visitor.visit(tree);
 
+    std::cout << "parse ok!\n";
     return 0;
 }
